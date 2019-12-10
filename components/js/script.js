55db2e3a5404fd31437710ddc3197f77e3ca5fb2
RedBloc <red@redbloc.ie>
2019-12-10 18:02:20 +0000

    first

---
+++ components/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+/*
+	$(".orgTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+	 $(this).addClass('selected');
+	 
+		$(".tab01").slideToggle();
+		$(".tab02").slideUp();
+		$(".tab03").slideUp();
+		})
+
+$(".raiseTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+ $(this).addClass('selected');
+ 
+		$(".tab02").slideToggle();
+		$(".tab01").slideUp();
+		$(".tab03").slideUp();
+		})
+
+$(".promoTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+ $(this).addClass('selected');
+		$(".tab03").slideToggle();
+		$(".tab02").slideUp();
+		$(".tab01").slideUp();
+		})
+	*/
+
+});
